a962c1d6d765871f759374201f93df4aa22631a0
Dana alsulami <danahxalsulami@gmail.com>
2022-06-15 20:54:18 +0300

    Update

--- Blockchain.js
+++ Blockchain.js
@@ -1,3 +1,19 @@
 class Blockchain{
-    
+    timestamp;
+    data;
+    hash;
+    previousHash;
+    height;
+    nonce;
+
+constructor(timestamp, data, hash, previousHash, height, nonce){
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.height = height;
+    nonce = 0; 
+}
+
+
 }
