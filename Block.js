54d41434e12530aaefccc23681b2d560c9c7ee04
Dana alsulami <danahxalsulami@gmail.com>
2022-06-18 23:43:48 +0300

    Homework

--- Block.js
+++ Block.js
@@ -1,24 +1,37 @@
-import { SHA256 } from "crypto-js";
+import sha256 from 'crypto-js/sha256.js';
 class Block{
-
-    timestamp;
-    data;
-    hash;
-    previousHash;
-    height;
-    nonce;
-
-constructor(timestamp, data, previousHash, height, nonce){
+constructor(height, timestamp, data, previousHash){
+    this.height = height;
     this.timestamp = timestamp;
     this.data = data;
-    this.hash = this.calculateHash();
     this.previousHash = previousHash;
-    this.height = height;
-    nonce = 0; 
-}
-calculateHash(){
-return SHA256(this.timestamp + this.data + this.previousHash + this.height).toString();
-}
+    this.hash = this.calculateHash();
+    this.nonce = 0; 
+   
 
 }
+calculateHash(){
+return  sha256( this.height + this.timestamp + this.data + this.previousHash + this.nonce).toString();
+}
+
+mine(difficulty){
+    let count = 0;
+ while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+        this.nonce++;
+        count++;
+        this.hash = this.calculateHash();
+ }
+ console.log("Block hashed at: " + count + " Hashes: " + this.hash);
+}
+}
+     /*for(let i=0; i<10000000; i++){
+    let hashed =sha256(this.timestamp + this.data + this.previousHash + this.height + this.nonce).toString();
+    console.log(hashed);
+    if (hashed.substring(0, this.difficulty) == "".padStart(difficulty, "0")){
+        console.log(hashed);
+    }
+
+    
+}
+*/
 export default Block;
--- Blockchain.js
+++ Blockchain.js
@@ -1,18 +1,42 @@
-import Block from "./Block";
+import Block from "./Block.js";
 
 class Blockchain{
 chain;
 
 constructor(){
-    this.chain = [];
-    this.init();
+    this.chain = [this.init()];
+    this.difficulty = 2;
+    
 }
+
 init(){
-this.chain(new Block(Date.now(), "Dana", "", "0", "0"));
-this.chain.push(Block);
-console.log(this.chain);
+   return new Block("0", "6/17/2022","G BLOCK", "");
+  
+}
+LastBlock(){
+    return this.chain[this.chain.length - 1];
 }
 
+addBlock(newBlock){
+    newBlock.previousHash = this.LastBlock().hash;
+   newBlock.mine(this.difficulty);
+    this.chain.push(newBlock);
+}
+validateBlockchain(){
+    for(let i = 1; i < this.chain.length; i++){
+        let currentBlock = this.chain[i];
+       let previosBlock = this.chain[i - 1];
+        if(currentBlock.hash !== currentBlock.calculateHash() || currentBlock.previosHash !== previosBlock.hash){
+            return false;
+        }
+    }
+    return true;
+}
 
 }
+let COIN = new Blockchain();
+COIN.addBlock(new Block(1, "6/18/2022", "BLOCK 1"));
+console.log(COIN);
+
+console.log("Is Blockchain valid?" + COIN.validateBlockchain());
 export default Blockchain;
